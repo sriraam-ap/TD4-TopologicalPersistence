3053fe105ff35bb2b6bc59da5193c1b3760f3b50
kazetof <fukasawakaze@gmail.com>
2024-10-19 19:42:35 +0900

    added function to caluculate boundary matrix https://github.com/sriraam-ap/TD4-TopologicalPersistence/issues/2

--- kazeto_example.py
+++ kazeto_example.py
@@ -1,10 +1,14 @@
 from persistence import read_filtration
 from persistence import convert_filtration_df
+from persistence import get_boundary_matrix
 
 
-filtration = read_filtration("./filtrations/filtration_D.txt")
+
+# filtration = read_filtration("./filtrations/filtration_D.txt")
+filtration = read_filtration("./tests/testcases/filtration_1.txt")
+
 s = filtration[0]
 
 import pandas as pd
 df = convert_filtration_df(filtration)
-
+bm = get_boundary_matrix(df)
--- persistence.py
+++ persistence.py
@@ -40,6 +40,52 @@ def convert_filtration_df(filtration: list) -> pd.DataFrame:
     df_sorted = df.sort_values(by=['val', 'dim'])
     return df_sorted
 
+def get_boundary_matrix(filtration_df: pd.DataFrame) -> list:
+    """
+    Parameters
+    ----------
+    filtration_df : pd.DataFrame
+    In [4]: filtration_df
+    Out[4]:
+    val  dim       vert
+    0  0.0    0        {0}
+    1  0.0    0        {1}
+    2  0.0    0        {2}
+    3  1.0    1     {0, 1}
+    4  1.0    1     {1, 2}
+    5  1.0    1     {0, 2}
+    6  2.0    2  {0, 1, 2}
+
+    Returns
+    -------
+    boundary_matrix : list
+    In [2]: boundary_matrix
+    Out[2]: 
+    [[0, 0, 0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0]]
+    """
+    N = len(filtration_df)
+    boundary_matrix = [[0 for i in range(N)] for j in range(N)]
+
+    for i, row in filtration_df.iterrows():
+        print(f"{i}/{len(filtration_df)}")
+        if row.dim == 0:
+            continue
+        else:
+            vert = row.vert
+            df_subset = filtration_df[filtration_df["dim"] == row.dim - 1]
+            df_subset = df_subset[df_subset.apply(lambda row: row["vert"].issubset(vert), axis=1)]
+            for idx in df_subset.index:
+                boundary_matrix[idx][i] = 1
+
+    return boundary_matrix
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Run Syntax: python persistence.py <path to filteration data>")
