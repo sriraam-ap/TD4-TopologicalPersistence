93f5b1d0b8714889d883072057e6a5c9dd943663
kazetof <fukasawakaze@gmail.com>
2024-10-24 19:02:30 +0900

    modifed example to compare two implementations

--- examples/example.py
+++ examples/example.py
@@ -2,7 +2,30 @@ from persistence.persistence import read_filtration
 from persistence.boundary_matrix import get_sparse_boundary_matrix
 from persistence.reduce_boundary_matrix import SparseBoundaryMatrixReducer
 # filtration = read_filtration("./tests/testcases/filtration_1.txt")
-filtration = read_filtration("./filtrations/filtration_D.txt")
+# filtration = read_filtration("./filtrations/filtration_D.txt")
+filtration = read_filtration("./filtrations/filtration_B.txt")
+# filtration = read_filtration("./filtrations/filtration_A.txt")
+
 sbm_col2row = get_sparse_boundary_matrix(filtration)
 sbm_reducer = SparseBoundaryMatrixReducer()
-sbm_reduced = sbm_reducer.reduce(sbm_col2row)
+sbm_col2row_1 = sbm_col2row.copy()
+sbm_col2row_2 = sbm_col2row.copy()
+
+
+import time
+
+
+start1 = time.time()
+sbm_reduced_1 = sbm_reducer.reduce1(sbm_col2row_1)
+t1 = time.time() - start1
+print(f"reduce1: {t1} sec")
+
+start2 = time.time()
+sbm_reduced_2 = sbm_reducer.reduce2(sbm_col2row_2)
+t2 = time.time() - start2
+
+print(f"reduce1: {t1} sec")
+print(f"reduce2: {t2} sec")
+
+# filtration B reduce1 reduction 1m16s
+# filtration B reduce2 kazeto reduction 15s
--- persistence/reduce_boundary_matrix.py
+++ persistence/reduce_boundary_matrix.py
@@ -9,7 +9,7 @@ class SparseBoundaryMatrixReducer(object):
     def __init__(self, verbose: bool=True):
         self._verbose = verbose # for debug
 
-    def reduce(self, sbm_col2row: dict) -> dict:
+    def reduce1(self, sbm_col2row: dict) -> dict:
         """
         We will change input and output format but now it's like below
 
@@ -44,54 +44,54 @@ class SparseBoundaryMatrixReducer(object):
         return sbm
                 
     
-    # def reduce(self, sbm_col2row: list, return_copy=True) -> dict:
-    #     """
-    #     Parameters
-    #     ----------
-    #     sbm_col2row : dict
-    #     return_copy: bool
-    #         It returns a different dict object if True.
-    #         It copies input sbm, then update it if False.
+    def reduce2(self, sbm_col2row: list, return_copy=True) -> dict:
+        """
+        Parameters
+        ----------
+        sbm_col2row : dict
+        return_copy: bool
+            It returns a different dict object if True.
+            It copies input sbm, then update it if False.
 
-    #     Example
-    #     -------
+        Example
+        -------
 
-    #     """
-    #     _sbm_col2row = sbm_col2row.copy() if return_copy else sbm_col2row
+        """
+        _sbm_col2row = sbm_col2row.copy() if return_copy else sbm_col2row
 
-    #     print("reduce: making row2col dict")
-    #     row2col = sbm_col2row_to_row2col(_sbm_col2row)
+        print("reduce: making row2col dict")
+        row2col = sbm_col2row_to_row2col(_sbm_col2row)
 
-    #     col_indices = list(_sbm_col2row.keys())
-    #     for _counter, j in enumerate(col_indices):
-    #         print(f"reduce: {_counter}/{len(col_indices)}")
+        col_indices = list(_sbm_col2row.keys())
+        for _counter, j in enumerate(col_indices):
+            # print(f"reduce: {_counter}/{len(col_indices)}")
 
-    #         if j in _sbm_col2row:
-    #             i = max(_sbm_col2row[j])
-    #         else:
-    #             continue # In this case, j col became all zeros after some addition
+            if j in _sbm_col2row:
+                i = max(_sbm_col2row[j])
+            else:
+                continue # In this case, j col became all zeros after some addition
 
-    #         col_indices_to_add = [_col_idx for _col_idx in row2col[i] if j < _col_idx]
-    #         col_indices_to_add = list(row2col[i])[1:]
-    #         print(f"number of col_indices_to_add: {len(col_indices_to_add)}")
-    #         for col_idx_to_add in col_indices_to_add:
-    #             nonzero_rows_at_j_col = _sbm_col2row[j] if j in _sbm_col2row else set()
-    #             nonzero_rows_at_col_to_add = _sbm_col2row[col_idx_to_add] if col_idx_to_add in _sbm_col2row else set()
-    #             xor = nonzero_rows_at_j_col ^ nonzero_rows_at_col_to_add
-    #             new_nonzero_rows = xor - nonzero_rows_at_col_to_add
+            col_indices_to_add = [_col_idx for _col_idx in row2col[i] if j < _col_idx]
+            col_indices_to_add = list(row2col[i])[1:]
+            print(f"number of col_indices_to_add: {len(col_indices_to_add)}")
+            for col_idx_to_add in col_indices_to_add:
+                nonzero_rows_at_j_col = _sbm_col2row[j] if j in _sbm_col2row else set()
+                nonzero_rows_at_col_to_add = _sbm_col2row[col_idx_to_add] if col_idx_to_add in _sbm_col2row else set()
+                xor = nonzero_rows_at_j_col ^ nonzero_rows_at_col_to_add
+                new_nonzero_rows = xor - nonzero_rows_at_col_to_add
 
-    #             # update col2row
-    #             if len(xor) == 0:
-    #                 if col_idx_to_add in _sbm_col2row:
-    #                     _sbm_col2row.pop(col_idx_to_add)
-    #             else:
-    #                 _sbm_col2row[col_idx_to_add] = xor
+                # update col2row
+                if len(xor) == 0:
+                    if col_idx_to_add in _sbm_col2row:
+                        _sbm_col2row.pop(col_idx_to_add)
+                else:
+                    _sbm_col2row[col_idx_to_add] = xor
 
-    #             # update row2col
-    #             for row_idx in new_nonzero_rows:
-    #                 if col_idx_to_add in row2col[row_idx]:
-    #                     row2col[row_idx].remove(col_idx_to_add)
-    #                 else:
-    #                     row2col[row_idx].add(col_idx_to_add)
+                # update row2col
+                for row_idx in new_nonzero_rows:
+                    if col_idx_to_add in row2col[row_idx]:
+                        row2col[row_idx].remove(col_idx_to_add)
+                    else:
+                        row2col[row_idx].add(col_idx_to_add)
 
-    #     return _sbm_col2row
+        return _sbm_col2row
